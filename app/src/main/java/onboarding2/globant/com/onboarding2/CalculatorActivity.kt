3eb2db1b0cfe090ea31eb04a93d5371973450a62
GLOBANT\taidy.marrugo <taidy.marrugo@globant.com>
2018-12-31 13:55:56 -0500

    Initial commit

---
+++ app/src/main/java/onboarding2/globant/com/onboarding2/CalculatorActivity.kt
@@ -0,0 +1,12 @@
+package onboarding2.globant.com.onboarding2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CalculatorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculator)
+    }
+}
