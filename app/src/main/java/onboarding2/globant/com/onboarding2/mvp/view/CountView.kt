0faae532909453e9a22f5105af775f8e6fd38a22
GLOBANT\taidy.marrugo <taidy.marrugo@globant.com>
2019-01-02 12:14:37 -0500

    countview created

--- app/src/main/java/onboarding2/globant/com/onboarding2/mvp/view/CountView.kt
+++ app/src/main/java/onboarding2/globant/com/onboarding2/mvp/view/CountView.kt
@@ -2,26 +2,66 @@ package com.globant.counter.mvp.view
 
 
 import android.app.Activity
-import com.globant.counter.utils.bus.observer.OnCountButtonPressedBusObserver
-import com.globant.counter.utils.bus.observer.OnResetButtonPressedBusObserver
+import android.text.TextUtils
 
 import kotlinx.android.synthetic.main.activity_calculator.*
+import onboarding2.globant.com.onboarding2.mvp.model.CalculatorITEM
 import onboarding2.globant.com.onboarding2.utils.bus.RxBus
+import onboarding2.globant.com.onboarding2.utils.bus.observer.*
 
 
 class CountView(activity: Activity) : ActivityView(activity) {
+    fun setExpression(expression: String) {
+        activity?.text_expression?.text = expression
+    }
 
     init {
-        activity.count_button.setOnClickListener {
-            RxBus.post(OnCountButtonPressedBusObserver.OnCountButtonPressed())
+        activity.btn_sum.setOnClickListener {
+            getInput()
+            RxBus.post(OnCalculatorActionItemPressedBusObserver.OnCalculatorActionButtonPressed(CalculatorITEM.SUMA))
+
         }
 
-        activity.reset_button.setOnClickListener {
-            RxBus.post(OnResetButtonPressedBusObserver.OnResetButtonPressed())
+        activity.btn_div.setOnClickListener {
+            getInput()
+            RxBus.post(OnCalculatorActionItemPressedBusObserver.OnCalculatorActionButtonPressed(CalculatorITEM.DIVS))
+
         }
+
+        activity.btn_multi.setOnClickListener {
+            getInput()
+            RxBus.post(OnCalculatorActionItemPressedBusObserver.OnCalculatorActionButtonPressed(CalculatorITEM.MULTIP))
+
+        }
+
+        activity.btn_rest.setOnClickListener {
+            getInput()
+            RxBus.post(OnCalculatorActionItemPressedBusObserver.OnCalculatorActionButtonPressed(CalculatorITEM.RESTA))
+
+        }
+
+        activity.btn_equal.setOnClickListener {
+            getInput()
+            RxBus.post(OnCalculatorEqualButtonPressedBusObserver.OnCalculatorEqualButtonPressed())
+        }
+
+        activity.btn_clear.setOnClickListener {
+            RxBus.post(OnCalculatorClearButtonPressedBusObserver.OnCalculatorClearButton())
+        }
+
     }
 
-    fun setCount(count: String) {
-        activity!!.count_label.text = count
+    private fun getInput() {
+        var input = activity?.input_number?.text.toString()
+        if (!TextUtils.isEmpty(input)) {
+            RxBus.post(OnCalculatorNumberButtonPressedBusObserver.OnCalculatorNumberButtonPressed(input.toFloat()))
+        }
+        activity?.input_number?.setText("")
     }
+
+    fun clear() {
+        activity?.input_number?.setText("")
+        activity?.text_expression?.text = ""
+    }
+
 }
