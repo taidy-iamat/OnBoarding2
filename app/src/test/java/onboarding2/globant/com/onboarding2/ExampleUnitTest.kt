c9c353142a9169a3b3ab20dfe02d4b59c63feefb
GLOBANT\taidy.marrugo <taidy.marrugo@globant.com>
2019-01-02 16:42:57 -0500

    Revert "unit test"
    This reverts commit 34071fd3dcd14bbbe7620916ffec6c718a741501.

--- app/src/test/java/onboarding2/globant/com/onboarding2/ExampleUnitTest.kt
+++ app/src/test/java/onboarding2/globant/com/onboarding2/ExampleUnitTest.kt
@@ -1,18 +1,13 @@
 package com.globant.counter
 
 
-
-
+import com.globant.counter.mvp.view.CountView
+import com.globant.counter.utils.bus.observer.OnCountButtonPressedBusObserver
+import com.globant.counter.utils.bus.observer.OnResetButtonPressedBusObserver
 import onboarding2.globant.com.onboarding2.CalculatorActivity
-import onboarding2.globant.com.onboarding2.mvp.model.CalculatorITEM
-import onboarding2.globant.com.onboarding2.mvp.model.CalculatorModel
 import onboarding2.globant.com.onboarding2.mvp.model.CountModel
 import onboarding2.globant.com.onboarding2.mvp.presenter.CountPresenter
-import onboarding2.globant.com.onboarding2.mvp.view.CountView
 import onboarding2.globant.com.onboarding2.utils.bus.RxBus
-import onboarding2.globant.com.onboarding2.utils.bus.observer.OnCalculatorActionItemPressedBusObserver
-import onboarding2.globant.com.onboarding2.utils.bus.observer.OnCalculatorEqualButtonPressedBusObserver
-import onboarding2.globant.com.onboarding2.utils.bus.observer.OnCalculatorNumberButtonPressedBusObserver
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
@@ -24,17 +19,16 @@ import org.mockito.Mockito.`when` as whenever
 class PresenterTest {
 
     private var presenter: CountPresenter? = null
-    private var model: CalculatorModel? = null
+    private var model: CountModel? = null
     @Mock
     lateinit var view: CountView
     @Mock
     lateinit var activity: CalculatorActivity
 
     @Before
-    fun setUp() {
+    fun setup() {
         MockitoAnnotations.initMocks(this)
-        model = CalculatorModel()
-
+        model = CountModel()
         // When
         whenever(view.activity).thenReturn(activity)
 
@@ -42,73 +36,27 @@ class PresenterTest {
     }
 
     @Test
+    fun isShouldIncCountByOne() {
+        model!!.reset();
+        RxBus.post(OnCountButtonPressedBusObserver.OnCountButtonPressed())
 
-    fun sumaTest() {
-        RxBus.post(OnCalculatorNumberButtonPressedBusObserver.OnCalculatorNumberButtonPressed(7.0f))
-        RxBus.post(OnCalculatorActionItemPressedBusObserver.OnCalculatorActionButtonPressed(CalculatorITEM.SUMA))
-        verify(view, times(3)).setExpression(" 7.0 +")
-
-        RxBus.post(OnCalculatorNumberButtonPressedBusObserver.OnCalculatorNumberButtonPressed(5.0f))
-
-        assertEquals(model?.action, CalculatorITEM.SUMA)
-
-        RxBus.post(OnCalculatorEqualButtonPressedBusObserver.OnCalculatorEqualButtonPressed())
-
-        assertEquals(model?.value1, 12.0f)
-        assertEquals(model?.value2, 0.0f)
-
+        val count = "1"
+        assertEquals(model!!.count, 1)
+        verify(view).setCount(count)
     }
 
     @Test
-    fun restaTest() {
-        RxBus.post(OnCalculatorNumberButtonPressedBusObserver.OnCalculatorNumberButtonPressed(10.0f))
-        RxBus.post(OnCalculatorActionItemPressedBusObserver.OnCalculatorActionButtonPressed(CalculatorITEM.RESTA))
-        verify(view, times(3)).setExpression(" 10.0 -")
+    fun isShouldResetCount() {
+        RxBus.post(OnCountButtonPressedBusObserver.OnCountButtonPressed())
+        RxBus.post(OnCountButtonPressedBusObserver.OnCountButtonPressed())
+        RxBus.post(OnCountButtonPressedBusObserver.OnCountButtonPressed())
+        var count = 3
+        assertEquals(model!!.count, count)
 
-        RxBus.post(OnCalculatorNumberButtonPressedBusObserver.OnCalculatorNumberButtonPressed(3.0f))
-
-        assertEquals(model?.action,CalculatorITEM.RESTA)
-
-        RxBus.post(OnCalculatorEqualButtonPressedBusObserver.OnCalculatorEqualButtonPressed())
-
-
-        assertEquals(model?.value1, 7.0f)
-        assertEquals(model?.value2, 0.0f)
+        RxBus.post(OnResetButtonPressedBusObserver.OnResetButtonPressed())
+        count = 0
+        assertEquals(model!!.count, count)
+        val invocations = 4
+        verify(view, times(invocations)).setCount(anyString());
     }
-
-    @Test
-    fun multiOperationTest() {
-        RxBus.post(OnCalculatorNumberButtonPressedBusObserver.OnCalculatorNumberButtonPressed(7.0f))
-        RxBus.post(OnCalculatorActionItemPressedBusObserver.OnCalculatorActionButtonPressed(CalculatorITEM.MULTIP))
-        verify(view, times(3)).setExpression(" 7.0 *")
-
-        RxBus.post(OnCalculatorNumberButtonPressedBusObserver.OnCalculatorNumberButtonPressed(4.0f))
-
-        assertEquals(model?.action, CalculatorITEM.MULTIP)
-
-        RxBus.post(OnCalculatorEqualButtonPressedBusObserver.OnCalculatorEqualButtonPressed())
-
-
-        assertEquals(model?.value1, 28.0f)
-        assertEquals(model?.value2, 0.0f)
-    }
-
-    @Test
-    fun divOperationTest() {
-        RxBus.post(OnCalculatorNumberButtonPressedBusObserver.OnCalculatorNumberButtonPressed(25.0f))
-        RxBus.post(OnCalculatorActionItemPressedBusObserver.OnCalculatorActionButtonPressed(CalculatorITEM.DIVS))
-        verify(view, times(3)).setExpression(" 5.0 /")
-
-        RxBus.post(OnCalculatorNumberButtonPressedBusObserver.OnCalculatorNumberButtonPressed(5.0f))
-
-        assertEquals(model?.action, CalculatorITEM.DIVS)
-
-        RxBus.post(OnCalculatorEqualButtonPressedBusObserver.OnCalculatorEqualButtonPressed())
-
-
-        assertEquals(model?.value1, 5.0f)
-        assertEquals(model?.value2, 0.0f)
-    }
-
-
 }
