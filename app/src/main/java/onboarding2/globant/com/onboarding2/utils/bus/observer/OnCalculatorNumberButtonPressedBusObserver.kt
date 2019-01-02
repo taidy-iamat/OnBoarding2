b71c4315eef9324b4963fe0ccd9ba0f25271b737
GLOBANT\taidy.marrugo <taidy.marrugo@globant.com>
2019-01-02 16:46:38 -0500

    Revert "create bus for actions buttons"
    This reverts commit 833a785e7e647a4a335450957c2f4aae4e5f4470.

--- app/src/main/java/onboarding2/globant/com/onboarding2/utils/bus/observer/OnCalculatorNumberButtonPressedBusObserver.kt
+++
@@ -1,9 +0,0 @@
-package onboarding2.globant.com.onboarding2.utils.bus.observer
-
-import com.globant.counter.utils.bus.observer.BusObserver
-
-abstract class OnCalculatorNumberButtonPressedBusObserver : BusObserver<OnCalculatorNumberButtonPressedBusObserver.OnCalculatorNumberButtonPressed>
-(OnCalculatorNumberButtonPressed::class.java) {
-
-    class OnCalculatorNumberButtonPressed (var number: Float = 0.0F)
-}
