f5166f2dcdeb7c593b36f86bdb05c8665ef2c34c
GLOBANT\taidy.marrugo <taidy.marrugo@globant.com>
2019-01-02 11:37:23 -0500

    Revert "refactor view for calculator operation and new model to calculator"
    This reverts commit eb96cf7af67877a1a3bbf69fded0e00b762ae75e.

--- app/src/main/java/onboarding2/globant/com/onboarding2/CalculatorActivity.kt
+++ app/src/main/java/onboarding2/globant/com/onboarding2/CalculatorActivity.kt
@@ -3,7 +3,6 @@ package onboarding2.globant.com.onboarding2
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.globant.counter.mvp.view.CountView
-import onboarding2.globant.com.onboarding2.mvp.model.CalculatorModel
 import onboarding2.globant.com.onboarding2.mvp.model.CountModel
 import onboarding2.globant.com.onboarding2.mvp.presenter.CountPresenter
 import onboarding2.globant.com.onboarding2.utils.bus.RxBus
@@ -16,7 +15,7 @@ class CalculatorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculator)
-        presenter = CountPresenter(CalculatorModel(), CountView(this))
+        presenter = CountPresenter(CountModel(), CountView(this))
     }
 
     override fun onDestroy() {
